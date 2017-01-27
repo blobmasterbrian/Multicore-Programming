210cc1160e9d68597572fded2ce8b27935a7c7c7
Brian Quinn <bjq207@nyu.edu>
2017-01-26 19:54:00 -0500

    test case * 10

--- lab0/test.cpp
+++ lab0/test.cpp
@@ -5,19 +5,17 @@
 using namespace set;  // make things easier
 // no use of std because of possible set conflicts
 
-int main()
-{
-    Set<int> intSet;             // set for test with integers
-    Set<char> charSet;           // set for test with characters
-
-
+void intTests() {
     // integer set testing
+    Set<int> intSet;             // set for test with integers
     std::vector<int> vec(201);   // create vector to hold 0-200
+
     for (int i = 0; i < vec.size(); ++i) {
         vec[i] = i;              // set values to 0-200
     }
 
-    std::default_random_engine gen;                 // generator
+    std::random_device rd;                          // seed
+    std::mt19937 gen(rd());                         // generator
     std::uniform_int_distribution<int> dis(0,200);  // distribution
 
     std::cout << "Inserting: ";
@@ -38,22 +36,26 @@ int main()
         std::cout << "Result: " << res << std::endl << std::endl;
     }
 
-    int num = 27;
-    std::cout << "Insert 27: " << intSet.insert(num) << std::endl;    // insert 27
-    std::cout << "Reinsert 27: " << intSet.insert(num) << std::endl;  // proves it can be 'reinserted'
-    std::cout << "Find 27: " << intSet.find(num) << std::endl;        // proves it can be found
-    std::cout << "Delete 27: " << intSet.del(num) << std::endl;       // proves it can be deleted
-    std::cout << "Find 27: " << intSet.find(num) << std::endl;        // proves it cannot be found after deletion
-    std::cout << std::endl << std::endl;
+    int num = dis(gen);
+    std::cout << "Insert " << num << ": " << intSet.insert(num) << std::endl;    // insert 27
+    std::cout << "Reinsert " << num << ": " << intSet.insert(num) << std::endl;  // proves it can be 'reinserted'
+    std::cout << "Find " << num << ": " << intSet.find(num) << std::endl;        // proves it can be found
+    std::cout << "Delete " << num << ": " << intSet.del(num) << std::endl;       // proves it can be deleted
+    std::cout << "Find " << num << ": " << intSet.find(num) << std::endl;        // proves it cannot be found after deletion
+    std::cout << std::endl;
+}
 
-
+void charTests() {
     // character set testing
+    Set<char> charSet;              // set for test with characters
     std::vector<char> charVec(95);  // create vector to hold character (ASCII 32-126)
+
     for (int i = 0; i < charVec.size(); ++i) {
         charVec[i] = i + 32;        // set values to corresponding character
     }
 
-    std::default_random_engine char_gen;                  // generator
+    std::random_device rd;                          // seed
+    std::mt19937 char_gen(rd());                         // generator
     std::uniform_int_distribution<int> char_dis(32,127);  // distribution
 
     std::cout << "Inserting: ";
@@ -74,11 +76,19 @@ int main()
         std::cout << "Result: " << res << std::endl << std::endl;
     }
 
-    char sym = '#';
-    std::cout << "Insert #: " << charSet.insert(sym) << std::endl;    // insert >
-    std::cout << "Reinsert #: " << charSet.insert(sym) << std::endl;  // proves it can be 'reinserted'
-    std::cout << "Find #: " << charSet.find(sym) << std::endl;        // proves it can be found
-    std::cout << "Delete #: " << charSet.del(sym) << std::endl;       // proves it can be deleted
-    std::cout << "Find #: " << charSet.find(sym) << std::endl;        // proves it cannot be found after deletion
-    std::cout << std::endl << std::endl;
+    char sym = char_dis(char_gen);
+    std::cout << "Insert " << sym << ": " << charSet.insert(sym) << std::endl;    // insert >
+    std::cout << "Reinsert " << sym << ": " << charSet.insert(sym) << std::endl;  // proves it can be 'reinserted'
+    std::cout << "Find " << sym << ": " << charSet.find(sym) << std::endl;        // proves it can be found
+    std::cout << "Delete " << sym << ": " << charSet.del(sym) << std::endl;       // proves it can be deleted
+    std::cout << "Find " << sym << ": " << charSet.find(sym) << std::endl;        // proves it cannot be found after deletion
+    std::cout << std::endl;
+}
+
+int main()
+{
+    for (size_t i = 0; i < 10; ++i) {
+        intTests();
+        charTests();
+    }
 }
